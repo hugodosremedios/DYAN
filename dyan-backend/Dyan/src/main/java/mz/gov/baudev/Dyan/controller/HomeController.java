a6aa9326db0d466a21e6f8a74adbb66b33fdc12a
hmagaia <hugo.magaia@cedsif.gov.gov.mz>
2025-11-30 13:47:12 +0200

    chore: initial spring boot project eBAU

---
+++ dyan-backend/Dyan/src/main/java/mz/gov/baudev/Dyan/controller/HomeController.java
@@ -0,0 +1,12 @@
+package mz.gov.baudev.Dyan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Olá, eBAU!";
+    }
+}
